2d867ad3110978c5f2f6e2c4b58e14818ae8dc7f
bruce1001 <brucegham@gmail.com>
2019-02-11 20:29:31 -0800

    creating main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+
+sys.path.append('/Users/bruce/Library/Colors')
+import config
+print config.DATABASE_CONFIG['DATABASE']
+
+# def main():
+# #     print config.DATABASE_CONFIG['host']
+#
+# if __name__ == "__main__":
+#     main()
